cba66a1edc3d13b474e2f6182a95c6f5e3665da5
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-03-20 10:49:00 -0600

    informacion de usuario mediante signals

--- src/app/signals/pages/conuter-page/conuter-page.component.ts
+++ src/app/signals/pages/conuter-page/conuter-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './conuter-page.component.html',
   styleUrl: './conuter-page.component.css'
 })
 export class ConuterPageComponent {
+
+  public counter = signal(10);
+                              //? suquareCounter siempre va a estar al pendiente de las señales dentro de su funcion computaada de modo que
+  public squareCounter= computed( () => this.counter() * this.counter() ); //? si es que estas cambian el tambien lo hara
+                                               //? es de solo lectura
+
+
+  increaseBy(value : number) {
+
+this.counter.update( current => current + value)
+  //? funcion de actualizacion, tiene el valor actual de la señal y lo que sea que regreses sera el valor actualizado
+}
 
 }
--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UserServiceService } from '../../services/userService.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+
+
+
+  private userService = inject(UserServiceService);
+  public userId = signal(1);
+
+
+  public currentUser =signal<User|undefined>(undefined);
+  public userWasFound =signal(true);
+
+
+ ngOnInit(): void {
+   this.loadUser(this.userId())
+  }
+
+  loadUser(id : number) {
+        if(id <= 0) return ;
+
+        this.userId.set(id);
+        this.currentUser.set(undefined);
+
+        this.userService.getUserById(id)
+        .subscribe(user => {
+          this.currentUser.set(user);
+        })  //? cuando no nos interesa el valor anterior hay que usar el set
+
+  }
 
 }
